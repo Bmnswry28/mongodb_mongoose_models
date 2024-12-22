9d38ac43e40d50bca6b0b9b987b0c9a640929665
abbas <bmnswry27@gmail.com>
2024-12-22 16:58:27 +0330

    all url

--- config/allowedOrigins.js
+++
@@ -1,8 +0,0 @@
-const allowedOrigins = [
-    'http://127.0.0.1:3000',
-    'http://localhost:3000',
-    'http://localhost:3500',
-    'http://127.0.0.1:3500',
-];
-
-module.exports = allowedOrigins;
